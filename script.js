289cb66f34ab4e7ebaa678bd4d6dfdd366acd0c0
AshishIT13 <ay87896@gmail.com>
2025-01-08 10:58:08 +0530

    Updated

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-import ollama from "ollama";
-
-async function runChat() {
-  try {
+const fs=require('fs');
+const { default:ollama }=require("ollama"); 
+async function runChat(){
+  try{
+    const inputFilePath="q.txt"
+    const inputContent=fs.readFileSync(inputFilePath, "utf-8")
     const response = await ollama.chat({
-      model: "qwen2:0.5b",
-      messages: [{ role: 'user', content: "Write product descriptions" }]
-    });
-
-    console.log("Chatbot Response:", response.message.content);
-  } catch (error) {
-    console.error("Error occurred:", error.message);
+      model:"qwen2:0.5b",
+      messages:[{role:"user",content:inputContent}]
+    })
+    const chatbotResponse=response.message.content
+    const outputFilePath="a.txt"
+    fs.writeFileSync(outputFilePath, chatbotResponse,"utf-8")
+    console.log("Chatbot response has been saved to output.txt.")
+  } 
+  catch (error){
+    console.error("Error occurred:",error.message)
   }
 }
-
-runChat();
+runChat()
